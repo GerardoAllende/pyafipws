5eddac4ccbf77a9cdd71373ba544ba60ef062d57
Gerardo Allende <gerardoallende@gmail.com>
2025-03-03 13:47:11 -0300

    Added import_faltantes.py because py2exe fail to include required .pyc in library.zip

---
+++ import_faltantes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+# for more details.
+
+# Based on MultipartPostHandler.py (C) 02/2006 Will Holcomb <wholcomb@gmail.com>
+# Ejemplos iniciales gracias a "Matias Gieco matigro@gmail.com"
+
+"Módulo para forzar la inclusión de módulos en library.zip con py2exe"
+from pyafipws import cot
+from pyafipws import wsfecred
+from pyafipws import ws_sr_padron
+from pyafipws import pyqr
+from pyafipws import padron
