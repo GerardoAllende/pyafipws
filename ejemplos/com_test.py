3ec41d83948f155581c1d1cf92194981ddcb3b9d
GerardoAllende <gerardoallende@gmail.com>
2025-03-06 18:39:06 -0300

    /ejemplos/com_test.py added wsct, wsfexv1, wsmtx

--- ejemplos/com_test.py
+++ ejemplos/com_test.py
@@ -11,18 +11,39 @@ def com_test():
     """
     wsaa = win32com.client.Dispatch("WSAA")
     print("WSAA", wsaa.Version, wsaa.InstallDir)
+    
     wsfev1 = win32com.client.Dispatch("WSFEv1")
     print("\nWSFEv1", wsfev1.Version)
     print("Conectar: ", wsfev1.Conectar())
     print_dummy(wsfev1)
+    
+    wsfexv1 = win32com.client.Dispatch("WSFEXv1")
+    print("\nWSFEXv1", wsfexv1.Version)
+    print("Conectar: ", wsfexv1.Conectar())
+    print_dummy(wsfexv1)
+    
+    wsmtx = win32com.client.Dispatch("WSMTXCA")
+    print("\nWSMTXCA", wsmtx.Version)
+    print("Conectar: ", wsmtx.Conectar())
+    
+    wsct = win32com.client.Dispatch("WSCT")
+    print("\nWSCT", wsct.Version)
+    print("Conectar: ", wsct.Conectar())
+    
     cot = win32com.client.Dispatch("COT")
     print("\nCOT", cot.Version)
     pyqr = win32com.client.Dispatch("PyQR")
+    
     print("\nPyQR", pyqr.Version)
     cred = win32com.client.Dispatch("WSFECred")
+    
     print("\nWSFECred", cred.Version)
     print("Conectar: ", cred.Conectar())
     print_dummy(cred)
+    
+
+    
+    
 
 if __name__ == "__main__":
     # Initialize COM
